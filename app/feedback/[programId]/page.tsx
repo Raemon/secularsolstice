92665a3496ccc1835c09c20d17edba6b586abf50
Raemon <raemon777@gmail.com>
2025-12-10 00:10:46 -0800

    Update VoteWidget to correct weight values for singability options

---
+++ app/feedback/[programId]/page.tsx
@@ -0,0 +1,7 @@
+import { ProgramFeedback } from "../ProgramFeedback";
+
+const ProgramFeedbackPage = ({params}:{params:{programId:string}}) => {
+  return <ProgramFeedback initialProgramId={params.programId} />;
+};
+
+export default ProgramFeedbackPage;
