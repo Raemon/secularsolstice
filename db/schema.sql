21013831731364e450a50ff9bdbddf6ecd86910f
Raemon <raemon777@gmail.com>
2026-01-01 11:32:09 -0800

    add program versions

--- db/schema.sql
+++ db/schema.sql
@@ -18,6 +18,28 @@ CREATE INDEX if not exists comments_privacy_idx on comments USING btree (privacy
 CREATE INDEX if not exists comments_user_id_idx on comments USING btree (user_id);
 CREATE INDEX if not exists comments_version_id_idx on comments USING btree (version_id);
 
+create table if not exists program_versions (
+  id uuid not null default gen_random_uuid(),
+  program_id uuid not null,
+  title text not null,
+  element_ids uuid[] not null default '{}'::uuid[],
+  archived boolean not null default false,
+  program_ids uuid[] not null default '{}'::uuid[],
+  video_url text,
+  print_program_foreword text,
+  print_program_epitaph text,
+  is_subprogram boolean not null default false,
+  locked boolean not null default false,
+  created_at timestamptz not null default now(),
+  constraint program_versions_pkey PRIMARY KEY (id),
+  constraint program_versions_program_id_fkey FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE
+);
+
+CREATE INDEX if not exists program_versions_archived_idx on program_versions USING btree (archived);
+CREATE INDEX if not exists program_versions_lower_title_idx on program_versions USING btree (lower(title));
+CREATE INDEX if not exists program_versions_program_id_idx on program_versions USING btree (program_id);
+CREATE INDEX if not exists program_versions_title_idx on program_versions USING btree (title);
+
 create table if not exists programs (
   id uuid not null default gen_random_uuid(),
   title text not null,
