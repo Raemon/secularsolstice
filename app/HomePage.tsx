4c409b1cdc3c918799fce90230e1b79028b214c9
Raemon <raemon777@gmail.com>
2026-01-02 00:20:34 -0800

    style

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { marked } from 'marked';
 import RecentSongs from './RecentSongs';
 import RecentPrograms from './RecentPrograms';
+import Link from 'next/link';
 
 const HomePage = () => {
   const [content, setContent] = useState<string>('');
@@ -33,11 +34,11 @@ const HomePage = () => {
       </div>
       <div className="lg:w-1/2 max-w-xl flex flex-col gap-12 pb-12 pt-20">
         <div>
-          <h3 className="font-georgia text-3xl mb-2 border-b border-gray-500 pb-2">Songs/Speeches</h3>
+          <Link href="/songs" className="font-georgia text-white hover:text-white/80 text-3xl mb-2 pb-2 block">Songs/Speeches</Link>
           <RecentSongs />
         </div>
         <div>
-          <h3 className="font-georgia text-3xl mb-2 border-b border-gray-500 pb-2">Programs</h3>
+          <Link href="/programs" className="font-georgia text-white hover:text-white/80 text-3xl mb-2 pb-2 block">Programs</Link>
           <RecentPrograms />
         </div>
       </div>
