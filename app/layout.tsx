eb00c45f55dd87f3549974a39f17c291cc88e0ca
Raemon <raemon777@gmail.com>
2026-01-03 13:18:18 -0800

    hide overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${ebGaramond.variable} antialiased bg-[#11101b] text-gray-100 pt-16`}
+        className={`${geistSans.variable} ${geistMono.variable} ${ebGaramond.variable} antialiased bg-[#11101b] text-gray-100 pt-[45px]`}
       >
         <UserProvider>
           <ConditionalHeader />
