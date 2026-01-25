bf02a9d45868c1bfee0096f35f8a629caaa623b4
Raemon <raemon777@gmail.com>
2026-01-24 22:56:54 -0800

    redirect

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
   
   // Turbopack configuration (Next.js 16 default)
   turbopack: {},
+  async redirects() {
+    return [
+      { source: '/resources', destination: 'https://secularsolstice.wordpress.com/resources', permanent: true },
+      { source: '/musical-arcs-and-goals', destination: 'https://secularsolstice.wordpress.com/musical-arcs-and-goals', permanent: true },
+      { source: '/a-campbellian-perspective-on-solstice', destination: 'https://secularsolstice.wordpress.com/a-campbellian-perspective-on-solstice', permanent: true },
+      { source: '/solstice-resource-repository', destination: 'https://secularsolstice.wordpress.com/solstice-resource-repository', permanent: true },
+      { source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug*', destination: 'https://secularsolstice.wordpress.com/:year/:month/:day/:slug*', permanent: true },
+    ];
+  },
 };
 
 export default nextConfig;
